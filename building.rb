a298533d59c2f15d5e60d066cbd1a69b1c4229ab
Ye Chuah <yechuah@gmail.com>
2012-09-28 19:59:09 +1000

    Update building.rb
    added rooms

--- building.rb
+++ building.rb
@@ -8,7 +8,7 @@ roomtype "DentalSurgery"
 # Construct building
 department "Mental Health", :travel =>  percent(20), :engineering =>  percent(20)
 
-room 1000..1008, :type => Bed
+room 1000..10010, :type => Bed
 room 1100..1120, :type => DirtyUtility1
 
 department 'Dental', :travel =>  percent(20), :engineering =>  percent(20)
